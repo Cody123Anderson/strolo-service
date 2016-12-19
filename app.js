a7704597f020d0152489d2917cf04c189084dc32
Cody Anderson <cody.anderson@insidesales.com>
2016-12-19 16:17:16 -0700

    Added CORS configuration

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.json({ limit: '20mb' })); // increases max file size
 app.use(bodyParser.urlencoded({limit: '20mb', extended: true})); // increases max file size
 
+// CORS configuration
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 routes(app);
 
 app.listen(port, (err) => {
