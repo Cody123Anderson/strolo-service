f385beef17d84de9e38f3b317cfa34e7974d1a7c
Cody Anderson <cody.anderson@insidesales.com>
2017-03-10 09:20:42 -0700

    removed config from git

--- config/index.js
+++
@@ -1,8 +0,0 @@
-/** Set the node environment variables */
-module.exports = {
-  JWT_SECRET: 'aldj09230falkfj132013ifjldkfa',
-  MONGO_URL: 'mongodb://localhost:27017/serenade',
-  PORT: 3000,
-  SENDGRID_API_KEY: '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',
-  SENDGRID_LIST_ID: 793453
-};
