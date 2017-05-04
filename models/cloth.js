55558c44533871d13fd191f67f311805e0912107
Cody Anderson <cody123anderson@gmail.com>
2017-05-04 11:06:11 -0600

    removed unused models

--- models/cloth.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var clothSchema = Schema({
-  name: { type: String, required: true }
-});
-
-// create the model for Cloth and expose it to our app
-module.exports = mongoose.model('Cloth', clothSchema);
